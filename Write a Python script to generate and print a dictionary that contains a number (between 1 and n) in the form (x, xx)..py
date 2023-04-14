623b2ac7b793dfb33f32f09727e4c10f2fbd76b0
Fahim Shahriar <tfahim645@gmail.com>
2023-04-14 23:13:41 +0600

    Dict 04

---
+++ Write a Python script to generate and print a dictionary that contains a number (between 1 and n) in the form (x, xx)..py
@@ -0,0 +1,8 @@
+# A Python script to generate and print a dictionary that contains a number (between 1 and n) in the form (x, x*x).
+def exponent (number):
+    dic = {}
+    for i in range(number):
+        dic[i+1] = (i+1)*(i+1)
+    return dic
+input_number = int(input("Please enter the number you want to see: "))
+print(f'Final out put is {exponent(input_number)}')
